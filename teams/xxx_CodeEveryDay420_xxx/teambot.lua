0c4ae05fa7a566c22cc147aeda5a93552a0014de
Walden Atte <awalden@da5-cs-b221-13.helsinki.fi>
2016-05-27 12:05:57 +0300

    Fixed grouping bug

--- teams/xxx_CodeEveryDay420_xxx/teambot.lua
+++ teams/xxx_CodeEveryDay420_xxx/teambot.lua
@@ -46,106 +46,106 @@ function object:SetTeamTarget(target)
   unitTeamTarget = target
 end
 
-object.nNextPushTime = 35000
-
-function object:GroupAndPushLogic()
-    
-  local nCurrentMatchTime = HoN.GetMatchTime()
-  local nCurrentGameTime = HoN.GetGameTime()
-    
-  if self.nPushState == STATE_IDLE then
-    
-    if nCurrentMatchTime > self.nNextPushTime then
-      --determine target lane
-      local sLane = nil
-      local tLaneUnits = {}
-      local tLaneNodes = {}
-      
-      sLane, tLaneUnits, tLaneNodes = self:RandomPushLane()
-      
-      local unitTarget = core.GetClosestLaneTower(tLaneNodes, core.bTraverseForward, core.enemyTeam)
-      if unitTarget == nil then
-        unitTarget = core.enemyMainBaseStructure
-      end
-      self.unitPushTarget = unitTarget
-      
-      --calculate estimated time to arrive
-      local unitRallyBuilding = core.GetFurthestLaneTower(tLaneNodes, core.bTraverseForward, core.myTeam)
-      if unitRallyBuilding == nil then
-        unitRallyBuilding = core.allyMainBaseStructure
-      end
-      self.unitRallyBuilding = unitRallyBuilding
-      
-      --invalidate our wait timeout
-      self.nGroupWaitTime = nil
-      
-      local vecTargetPos = unitRallyBuilding:GetPosition()
-      for key, hero in pairs(tLaneUnits) do
-        if hero:IsBotControlled() then
-          local nWalkTime = core.TimeToPosition(vecTargetPos, hero:GetPosition(), hero:GetMoveSpeed())
-          local nRespawnTime = (not hero:IsAlive() and hero:GetRemainingRespawnTime()) or 0
-          local nTotalTime = nWalkTime * self.nGroupEstimateMul + nRespawnTime
-          tinsert(self.tArrivalEstimatePairs, {hero, nTotalTime})
-        end
-      end
-      
-      self.nPushStartTime = nCurrentMatchTime
-      self.nPushState = STATE_GROUPING
-    end
-  elseif self.nPushState == STATE_GROUPING then
-    if not self.unitRallyBuilding or not self.unitRallyBuilding:IsValid() then
-      self.nNextPushTime = nCurrentMatchTime
-      self.nPushState = STATE_IDLE
-    elseif self.nGroupWaitTime ~= nil and nCurrentGameTime >= self.nGroupWaitTime then
-      self.nPushState = STATE_PUSHING
-    else
-      local bAllHere = true
-      local bAnyHere = false
-      local vecRallyPosition = self.unitRallyBuilding:GetPosition()
-      for key, tPair in pairs(self.tArrivalEstimatePairs) do
-        local unit = tPair[1]
-        local nTime = tPair[2]
-        if not unit or not nTime then 
-          BotEcho('GroupAndPushLogic - ERROR - malformed arrival esimate pair!')
-        end
-        
-        if Vector3.Distance2DSq(unit:GetPosition(), vecRallyPosition) > self.nGroupUpRadiusSq then
-        
-          if nCurrentMatchTime > self.nPushStartTime + nTime then
-            self.tArrivalEstimatePairs[key] = nil
-          else
-            bAllHere = false
-          end
-          
-        end
-      end
-      
-      if bAllHere then
-        self.nPushState = STATE_PUSHING
-      elseif bAnyHere and self.nGroupWaitTime == nil then
-        self.nGroupWaitTime = nCurrentGameTime + self.nMaxGroupWaitTime
-      end
-    end
-  elseif self.nPushState == STATE_PUSHING then
-    local bEnd = not self.unitPushTarget:IsAlive()
-    
-    if bEnd == false then
-      --if we don't want to end already, see if we have wiped
-      local nHeroesAlive = 0
-      for _, hero in pairs(self.tAllyHeroes) do
-        if hero:IsAlive() then
-          nHeroesAlive = nHeroesAlive + 1
-        end
-      end
-      
-      bEnd = nHeroesAlive <= 3
-    end
-    
-    if bEnd == true then
-      self:BuildLanes()
-      self.nPushState = STATE_IDLE
-      self.nNextPushTime = nCurrentMatchTime + 1000; -- Push right after regroup
-    end
-  end
-  
-end
+--object.nNextPushTime = 35000
+--
+--function object:GroupAndPushLogic()
+--    
+--  local nCurrentMatchTime = HoN.GetMatchTime()
+--  local nCurrentGameTime = HoN.GetGameTime()
+--    
+--  if self.nPushState == STATE_IDLE then
+--    
+--    if nCurrentMatchTime > self.nNextPushTime then
+--      --determine target lane
+--      local sLane = nil
+--      local tLaneUnits = {}
+--      local tLaneNodes = {}
+--      
+--      sLane, tLaneUnits, tLaneNodes = self:RandomPushLane()
+--      
+--      local unitTarget = core.GetClosestLaneTower(tLaneNodes, core.bTraverseForward, core.enemyTeam)
+--      if unitTarget == nil then
+--        unitTarget = core.enemyMainBaseStructure
+--      end
+--      self.unitPushTarget = unitTarget
+--      
+--      --calculate estimated time to arrive
+--      local unitRallyBuilding = core.GetFurthestLaneTower(tLaneNodes, core.bTraverseForward, core.myTeam)
+--      if unitRallyBuilding == nil then
+--        unitRallyBuilding = core.allyMainBaseStructure
+--      end
+--      self.unitRallyBuilding = unitRallyBuilding
+--      
+--      --invalidate our wait timeout
+--      self.nGroupWaitTime = nil
+--      
+--      local vecTargetPos = unitRallyBuilding:GetPosition()
+--      for key, hero in pairs(tLaneUnits) do
+--        if hero:IsBotControlled() then
+--          local nWalkTime = core.TimeToPosition(vecTargetPos, hero:GetPosition(), hero:GetMoveSpeed())
+--          local nRespawnTime = (not hero:IsAlive() and hero:GetRemainingRespawnTime()) or 0
+--          local nTotalTime = nWalkTime * self.nGroupEstimateMul + nRespawnTime
+--          tinsert(self.tArrivalEstimatePairs, {hero, nTotalTime})
+--        end
+--      end
+--      
+--      self.nPushStartTime = nCurrentMatchTime
+--      self.nPushState = STATE_GROUPING
+--    end
+--  elseif self.nPushState == STATE_GROUPING then
+--    if not self.unitRallyBuilding or not self.unitRallyBuilding:IsValid() then
+--      self.nNextPushTime = nCurrentMatchTime
+--      self.nPushState = STATE_IDLE
+--    elseif self.nGroupWaitTime ~= nil and nCurrentGameTime >= self.nGroupWaitTime then
+--      self.nPushState = STATE_PUSHING
+--    else
+--      local bAllHere = true
+--      local bAnyHere = false
+--      local vecRallyPosition = self.unitRallyBuilding:GetPosition()
+--      for key, tPair in pairs(self.tArrivalEstimatePairs) do
+--        local unit = tPair[1]
+--        local nTime = tPair[2]
+--        if not unit or not nTime then 
+--          BotEcho('GroupAndPushLogic - ERROR - malformed arrival esimate pair!')
+--        end
+--        
+--        if Vector3.Distance2DSq(unit:GetPosition(), vecRallyPosition) > self.nGroupUpRadiusSq then
+--        
+--          if nCurrentMatchTime > self.nPushStartTime + nTime then
+--            self.tArrivalEstimatePairs[key] = nil
+--          else
+--            bAllHere = false
+--          end
+--          
+--        end
+--      end
+--      
+--      if bAllHere then
+--        self.nPushState = STATE_PUSHING
+--      elseif bAnyHere and self.nGroupWaitTime == nil then
+--        self.nGroupWaitTime = nCurrentGameTime + self.nMaxGroupWaitTime
+--      end
+--    end
+--  elseif self.nPushState == STATE_PUSHING then
+--    local bEnd = not self.unitPushTarget:IsAlive()
+--    
+--    if bEnd == false then
+--      --if we don't want to end already, see if we have wiped
+--      local nHeroesAlive = 0
+--      for _, hero in pairs(self.tAllyHeroes) do
+--        if hero:IsAlive() then
+--          nHeroesAlive = nHeroesAlive + 1
+--        end
+--      end
+--      
+--      bEnd = nHeroesAlive <= 3
+--    end
+--    
+--    if bEnd == true then
+--      self:BuildLanes()
+--      self.nPushState = STATE_IDLE
+--      self.nNextPushTime = nCurrentMatchTime + 1000; -- Push right after regroup
+--    end
+--  end
+--  
+--end
